b25bfacf6b4a9793ca1c182aa0453eb2ce3677ef
Nguyen Thanh <thanhnguyennguyen1995@gmail.com>
2020-09-12 21:20:38 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-export default {
+window.plugin = 10000
+/*export default {
    install(Vue, config = { t: translate }) {
       Vue.mixin({
          beforeCreate() {
-            const filters = this.$options.filters;
+            const filters = this.$options.filters
             const set = (name) => {
                if (typeof filters[name] === 'undefined') {
                   filters[name] = (...args) => {
-                     return this.$i18n && this["$" + name] ? this["$" + name](args) : args[0];
+                     return this.$i18n && this["$" + name] ? this["$" + name](args) : args[0]
                   }
                   
                   if ( name in config && filter[ config[name] ] === undefined ) {
                      filters[ config[name] ] = config[ name ]
                   }
                }
-            };
-            set('t');
-            set('tc');
-            set('te');
+            }
+            set('t')
+            set('tc')
+            set('te')
          },
          beforeDestroy() {
-            var filters = this.$options.filters;
-            delete filters.t;
-            delete filters.te;
-            delete filters.tc;
+            var filters = this.$options.filters
+            delete filters.t
+            delete filters.te
+            delete filters.tc
             for ( let key in config ) {
                delete filters[ key ]
             }
          }
-      });
+      })
    }
-};
+}*/
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const {resolve} = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'vue-i18n-filters.js'
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        })
+    ]
+}
